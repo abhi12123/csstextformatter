87ecd0649a070a897154182c808ed27e82e20b45
abhinav <vpofficial55@gmail.com>
2022-01-13 22:03:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const sampleText = document.getElementById("sample-text");
+
+const unitSelectors = document.getElementsByClassName("unit-selector");
+const copyBtn = document.getElementById("copy-code");
+const cssCode = document.getElementById("css-code");
+
+const units = ["px", "rem", "em", "%", "in", "cm", "mm"];
+
+const words =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+const defaultValues = {
+  "font-size": 16,
+  color: "#000000",
+  "background-color": "none",
+  "text-align": "left",
+  //   "vertical-align":"baseline",
+  "text-decoration": "none",
+  "text-transform":"none",
+  "text-indent":0,
+  "letter-spacing":0,
+  "word-spacing":0,
+};
+
+const properties = Object.keys(defaultValues);
+//load css code from samplecode
+const loadCssCode = () => {
+  let newCssCode = "";
+  properties.map((property) => {
+    //converting css mode to js mode
+    const index = property.indexOf("-");
+    let convertedPropertyName = property;
+    if (index > -1) {
+      let propertyValue = property[index + 1].toUpperCase();
+      convertedPropertyName =
+        property.slice(0, index) + propertyValue + property.slice(index + 2);
+    }
+    if (sampleText.style[convertedPropertyName]) {
+      //adding style to css code
+      newCssCode +=
+        property + ":" + sampleText.style[convertedPropertyName] + ";" + "\n";
+    }
+  });
+  //saving css code
+  cssCode.innerText = newCssCode;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  //sets unit options
+  let innerHtml = "";
+  units.map((unit) => {
+    innerHtml += '<option value="' + unit + '">' + unit + "</option>";
+  });
+  [...unitSelectors].map((unitSelector) => {
+    unitSelector.innerHTML = innerHtml;
+  });
+
+  sampleText.innerText = words.slice(0, 300);
+  //get default values
+  loadCssCode();
+});
+
+//copy css code
+copyBtn.onclick = () => {
+  const codeToBeCopied = cssCode.innerText;
+  let emptyArea = document.getElementById("text-area");
+  emptyArea.innerHTML = codeToBeCopied;
+  emptyArea.select();
+  document.execCommand("copy");
+};
+
+const handleValueChangeWithUnit = (e) => {
+  sampleText.style[e.name] =
+    e.value + document.getElementById("unit-selector-" + e.name).value;
+  loadCssCode();
+};
+
+const handleUnitChange = (e) => {
+  sampleText.style[e.name] = document.getElementById(e.name).value + e.value;
+  loadCssCode();
+};
+
+const handleValueChange = (e) => {
+  sampleText.style[e.name] = e.value;
+  loadCssCode();
+};
+
+const handleReset = (e) => {
+  sampleText.style.removeProperty(e.name);
+  document.getElementById(e.name).value = defaultValues[e.name];
+  if (document.getElementById("unit-selector-" + e.name)) {
+    document.getElementById("unit-selector-" + e.name).value = "px";
+  }
+  loadCssCode();
+};
+
+const handleResetAll = () => {
+  properties.map((property) => {
+    sampleText.style.removeProperty(property);
+    if (document.getElementById(property)) {
+      document.getElementById(property).value = defaultValues[property];
+    }
+    if (document.getElementById("unit-selector-" + property)) {
+      document.getElementById("unit-selector-" + property).value = "px";
+    }
+  });
+  loadCssCode();
+};
+
+const handleSampleTextLength = (e) => {
+  sampleText.innerText = words.slice(0, e.value);
+};
